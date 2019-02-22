2916b8e01def7bd4d75106b1ac75a0eb1d841535
kunchaiCH <gun1one2@gmail.com>
2019-02-22 20:59:00 +0700

    Sequence function

--- function.cpp
+++ function.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
+void sequence(){            // Sequence function
+    int hrs;
+    cout << "Enter total hours: ";
+    cin >> hrs;
+    int days = hrs / 24;
+    int hours = hrs % 24;
+    cout << hrs << " hours = " << days << " days " << hours << " hours " << endl;
+}
 
 main(){
-
+    sequence();
     return 0;
 }
